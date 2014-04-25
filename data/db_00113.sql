842eae811441a7cc28163294944effc12929bfe7
Nick Wallace <nwallace@fyberstudios.com>
2014-04-25 15:40:59 +0000

    Added player teams

---
+++ data/db_00113.sql
@@ -0,0 +1,2 @@
+ALTER TABLE  `players` ADD  `player_team` VARCHAR( 255 ) NOT NULL ,
+ADD  `active` TINYINT NOT NULL DEFAULT  '1'
--- public_html/admin/inc/views/player_form.php
+++ public_html/admin/inc/views/player_form.php
@@ -36,9 +36,49 @@
                         <option value="OF" <?php if($P->position == "OF"): ?>selected<?php endif; ?>>OF</option>
                     </select>
                 </div>
+                <div class="form-group">
+                    <label for="player[player_team]">Player Team</label>
+                    <select class="form-control" id="player[player_team]" name="player[player_team]">
+                        <option>--Player Team--</option>
+                        <option value="Angels" <?php if($P->player_team == "Angels"): ?>selected<?php endif; ?>>Angels</option>
+                        <option value="Astros" <?php if($P->player_team == "Astros"): ?>selected<?php endif; ?>>Astros</option>
+                        <option value="Athletics" <?php if($P->player_team == "Athletics"): ?>selected<?php endif; ?>>Athletics</option>
+                        <option value="Blue Jays" <?php if($P->player_team == "Blue Jays"): ?>selected<?php endif; ?>>Blue Jays</option>
+                        <option value="Braves" <?php if($P->player_team == "Braves"): ?>selected<?php endif; ?>>Braves</option>
+                        <option value="Brewers" <?php if($P->player_team == "Brewers"): ?>selected<?php endif; ?>>Brewers</option>
+                        <option value="Cardinals" <?php if($P->player_team == "Cardinals"): ?>selected<?php endif; ?>>Cardinals</option>
+                        <option value="Cubs" <?php if($P->player_team == "Cubs"): ?>selected<?php endif; ?>>Cubs</option>
+                        <option value="Diamondbacks" <?php if($P->player_team == "Diamondbacks"): ?>selected<?php endif; ?>>Diamondbacks</option>
+                        <option value="Dodgers" <?php if($P->player_team == "Dodgers"): ?>selected<?php endif; ?>>Dodgers</option>
+                        <option value="Giants" <?php if($P->player_team == "Giants"): ?>selected<?php endif; ?>>Giants</option>
+                        <option value="Indians" <?php if($P->player_team == "Indians"): ?>selected<?php endif; ?>>Indians</option>
+                        <option value="Mariners" <?php if($P->player_team == "Mariners"): ?>selected<?php endif; ?>>Mariners</option>
+                        <option value="Marlins" <?php if($P->player_team == "Marlins"): ?>selected<?php endif; ?>>Marlins</option>
+                        <option value="Mets" <?php if($P->player_team == "Mets"): ?>selected<?php endif; ?>>Mets</option>
+                        <option value="Nationals" <?php if($P->player_team == "Nationals"): ?>selected<?php endif; ?>>Nationals</option>
+                        <option value="Orioles" <?php if($P->player_team == "Orioles"): ?>selected<?php endif; ?>>Orioles</option>
+                        <option value="Padres" <?php if($P->player_team == "Padres"): ?>selected<?php endif; ?>>Padres</option>
+                        <option value="Phillies" <?php if($P->player_team == "Phillies"): ?>selected<?php endif; ?>>Phillies</option>
+                        <option value="Pirates" <?php if($P->player_team == "Pirates"): ?>selected<?php endif; ?>>Pirates</option>
+                        <option value="Rangers" <?php if($P->player_team == "Rangers"): ?>selected<?php endif; ?>>Rangers</option>
+                        <option value="Rays" <?php if($P->player_team == "Rays"): ?>selected<?php endif; ?>>Rays</option>
+                        <option value="Red Sox" <?php if($P->player_team == "Red Sox"): ?>selected<?php endif; ?>>Red Sox</option>
+                        <option value="Reds" <?php if($P->player_team == "Reds"): ?>selected<?php endif; ?>>Reds</option>
+                        <option value="Rockies" <?php if($P->player_team == "Rockies"): ?>selected<?php endif; ?>>Rockies</option>
+                        <option value="Royals" <?php if($P->player_team == "Royals"): ?>selected<?php endif; ?>>Royals</option>
+                        <option value="Tigers" <?php if($P->player_team == "Tigers"): ?>selected<?php endif; ?>>Tigers</option>
+                        <option value="Twins" <?php if($P->player_team == "Twins"): ?>selected<?php endif; ?>>Twins</option>
+                        <option value="White Sox" <?php if($P->player_team == "White Sox"): ?>selected<?php endif; ?>>White Sox</option>
+                        <option value="Yankees" <?php if($P->player_team == "Yankees"): ?>selected<?php endif; ?>>Yankees</option>
+                    </select>
+                </div>
                 <div class="form-group checkbox">
                     <label for="dh">Player can DH</label>
                     <input type="checkbox" name="dh" id="dh" value="1" <?php if($P->dh == 1): ?>checked<?php endif; ?> />
+                </div>
+                <div class="form-group checkbox">
+                    <label for="active">Player is Active</label>
+                    <input type="checkbox" name="active" id="active" value="1" <?php if($P->active == 1): ?>checked<?php endif; ?> />
                 </div>
                 <button class="btn btn-success pull-right">Update</button>
             </form>
