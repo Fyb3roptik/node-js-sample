50fc71c6019e778e3ec9d5036a579abcc54bacc9
Nick Wallace <nwallace@fyberstudios.com>
2014-07-21 22:35:46 +0000

    Changed ARI team

--- scripts/lineups.php
+++ scripts/lineups.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/../public_html/inc/global.php';
 $LINEUP = new Lineup();
 $cache = new Cache();
 
-$cities = array("Angels" => "LAA", "Diamondbacks" => "ARI", "Braves" => "ATL", "Orioles" => "BAL", "Red Sox" => "BOS", "Cubs" => "CHC", "White Sox" => "CWS", "Reds" => "CIN", "Indians" => "CLE", "Rockies" => "COL", "Tigers" => "DET", "Marlins" => "MIA", "Astros" => "HOU", "Royals" => "KC", "Dodgers" => "LAD", "Brewers" => "MIL", "Twins" => "MIN", "Mets" => "NYM", "Yankees" => "NYY", "Athletics" => "OAK", "Phillies" => "PHI", "Pirates" => "PIT", "Padres" => "SD", "Giants" => "SF", "Mariners" => "SEA", "Cardinals" => "STL", "Rays" => "TB", "Rangers" => "TEX", "Blue Jays" => "TOR", "Nationals" => "WSH");
+$cities = array("Angels" => "LAA", "D-backs" => "ARI", "Braves" => "ATL", "Orioles" => "BAL", "Red Sox" => "BOS", "Cubs" => "CHC", "White Sox" => "CWS", "Reds" => "CIN", "Indians" => "CLE", "Rockies" => "COL", "Tigers" => "DET", "Marlins" => "MIA", "Astros" => "HOU", "Royals" => "KC", "Dodgers" => "LAD", "Brewers" => "MIL", "Twins" => "MIN", "Mets" => "NYM", "Yankees" => "NYY", "Athletics" => "OAK", "Phillies" => "PHI", "Pirates" => "PIT", "Padres" => "SD", "Giants" => "SF", "Mariners" => "SEA", "Cardinals" => "STL", "Rays" => "TB", "Rangers" => "TEX", "Blue Jays" => "TOR", "Nationals" => "WSH");
 
 foreach($cities as $team => $city) {
     $LIST[$city] = $LINEUP->getCurrentLineups($city);
