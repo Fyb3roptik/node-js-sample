c2d512922dc5d38a7744c592e22e5b960918d366
Nick Wallace <nwallace@fyberstudios.com>
2014-07-27 00:23:13 +0000

    Updated to trailer

--- public_html/inc/views/home.php
+++ public_html/inc/views/home.php
@@ -1,16 +1,34 @@
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Beast Franchise Tutorial</h4>
+      </div>
+      <div class="modal-body">
+        <iframe src="//player.vimeo.com/video/101363592?color=7537b1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/101363592">Beast Franchise tutorial</a> from <a href="http://vimeo.com/fyberstudios">Fyb3roptik</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="row top-row">
     <div class="col-lg-12 col-sm-12">
-        <h3>The First and Only Daily Fantasy Baseball Game where you set your batting order.</h3>
-        <h4>You are the GM. You are the Coach. You are the Legend</h4>
+        <h3>BEAST FRANCHISE:</h3>
+        <h3>THE DAILY BASEBALL GM EXPERIENCE</h3>
+        <iframe src="//player.vimeo.com/video/101148785?color=7537b1" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/101148785">BEAST FRANCHISE: Daily Fantasy Baseball GM Experience</a></p>
     </div>
 </div>
 
 <div class="row">
     <div class="col-lg-12 col-sm-12">
         <div class="page-header">
-            <h3>Step 1</h3>
+            <h3>Step 1 - THE GM EXPERIENCE</h3>
         </div>
-        <h4>Select your team <small>You are sitting in the GM Office. You look at all the available players and fill out your roster as if this was your own team.</small></h4>
+        <h4>You are the General Manager as you select your ultimate BEAST SQUAD.</h4>
         <h4>No Salary Cap, pick the Players you want at each position.</h4>
     </div>
 </div>
@@ -18,28 +36,21 @@
 <div class="row">
     <div class="col-lg-12 col-sm-12">
         <div class="page-header">
-            <h3>Step 2</h3>
+            <h3>Step 2 - THE COACH EXPERIENCE</h3>
         </div>
-        <h4>The Coach Experience <small>You are now in the coaches office. You examine your roster as you put together the best batting order you can.</small></h4>
-        <h4>Just like real baseball, every 3 Outs the bases clear. Your leadoff hitter can be a player playing at 4:00pm, and you will not know his next move until the 7:00pm game where your #2 hitter is waiting.</h4>
+        <h4>You are now in the coaches office.  You examine your roster and put together your BEAST BATTING ORDER (1-9).</h4>
+        <h4>Just like real baseball, every 3 Outs the bases clear. Your leadoff hitter can be a player playing at 4:00pm, and you will not know his next move until the 7:00pm game where your #2 hitter is waiting.  <button class="btn btn-primary" data-toggle="modal" data-target="#myModal">Watch the Tutorial to learn more</button></h4>
     </div>
 </div>
 
 <div class="row">
     <div class="col-lg-12 col-sm-12">
         <div class="page-header">
-            <h3>Step 3</h3>
+            <h3>Step 3 - THE OWNER EXPERIENCE</h3>
         </div>
-        <h4>Watch your BEAST FRANCHISE Fantasy Baseball team destroy the competition. Compete for Weekly/Monthly and soon Daily Prizes. Free to Enter.</h4>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12 col-sm-12">
-        <div class="page-header">
-            <h3>Video Tutorial</h3>
-        </div>
-        <iframe src="//player.vimeo.com/video/101148785?color=7537b1" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/101148785">BEAST FRANCHISE: Daily Fantasy Baseball GM Experience</a></p>
+        <h4>Watch your BEAST FRANCHISE Fantasy Baseball team destroy the competition.</h4>
+        <h4>Compete for Daily Prizes.</h4>
+        <h4>Free to Enter or play in a Cash games to win money and start your empire.</h4>
     </div>
 </div>
 
